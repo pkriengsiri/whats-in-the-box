2fa560bc748860ec62cdeddde4a12b80cb3d010b
Pete Kriengsiri <pkriengsiri@gmail.com>
2020-12-26 15:32:13 -0500

    Fixes #13

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,17 +1,48 @@
-console.log("hello World");
+$(document).ready(function () {
+  // DOM VARIABLES
 
+  // JS VARIABLES
+  var userBoxes = [] // Tracks all the boxes the user has packed
 
+  // FUNCTION DECLARATIONS
 
-//event listener to save input text into variables
-$("#print-label").on("click", function (e) {
-e.preventDefault();
-var userName = $("#user-name").val();
-var boxName = $("#box-name").val();
-var boxContent = $("#box-content").val();
-var formContent = userName +" "+ boxName + " "+ boxContent
-console.log(userName);
-console.log(boxName);
-console.log(boxContent);
-console.log(formContent);
+  // Handles the behavior once the user clicks the submit button
+  function generateQRCode(e) {
+    // Prevents default form behavior
+    e.preventDefault();
+    // Pulls text fields from form and stores them as variables
+    var userName = $("#user-name").val();
+    var boxName = $("#box-name").val();
+    var boxContent = $("#box-content").val();
+
+    // Call the paste API to box the box contents to a new paste
+    var queryURL =
+      "https://api.paste.ee/v1/pastes?key=aNXCh2y6HrufDC2QMvqoKW7in1uvG7AhUOP1Z4JGF&encrypted=false";
+    $.ajax({
+      url: queryURL,
+      type: "POST",
+      data: {
+        description: boxName,
+        sections: [
+          {
+            name: userName,
+            syntax: "autodetect",
+            contents: boxContent,
+          },
+        ],
+      },
+      success: function (response) {
+        // Get the paste ID from the submitted paste
+        var pasteID = response.id;
+        console.log(pasteID);
+      },
+    });
+  }
+
+  // FUNCTION CALLS
+
+  // EVENT HANDLERS
+  //event listener to save input text into variables
+  $("#print-label").on("click", generateQRCode);
 
 });
