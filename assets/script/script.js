23ac7e1a319b8d7f77b1aae078590ec7c31ac707
Juno Brown <amarbrown@gmail.com>
2020-12-26 14:08:49 -0500

    Closes #12

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,1 +1,17 @@
 console.log("hello World");
+
+
+
+//event listener to save input text into variables
+$("#print-label").on("click", function (e) {
+e.preventDefault();
+var userName = $("#user-name").val();
+var boxName = $("#box-name").val();
+var boxContent = $("#box-content").val();
+var formContent = userName +" "+ boxName + " "+ boxContent
+console.log(userName);
+console.log(boxName);
+console.log(boxContent);
+console.log(formContent);
+
+});
