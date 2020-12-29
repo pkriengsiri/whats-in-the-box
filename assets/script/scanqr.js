4e74450af822888202584aaf0239f42e76ba48c3
Pete Kriengsiri <pkriengsiri@gmail.com>
2020-12-28 20:52:43 -0500

    Fixes #23, added function to get box info using paste.ee API call, and populate box info on the DOM

--- assets/script/scanqr.js
+++ assets/script/scanqr.js
@@ -16,8 +16,27 @@ function getPasteID() {
     console.log(pasteID);
 }
 
+//Pulls the box info from the paste and sets the content on the DOM
+function getBoxInfo() {
+    var queryURL = "https://api.paste.ee/v1/pastes/" + pasteID + "/?key=aNXCh2y6HrufDC2QMvqoKW7in1uvG7AhUOP1Z4JGF"
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+        }).then(function(response) {
+
+          boxName = response.paste.description;
+          userName = response.paste.sections[0].name;
+          boxContents = response.paste.sections[0].contents;
+
+          $("#user-name").text(userName);
+          $("#box-name").text(boxName);
+          $("#box-contents").text(boxContents);
+        });
+}
+
 //FUNCTION CALLS
 getPasteID();
+getBoxInfo();
 
 //EVENT HANDLERS
 
