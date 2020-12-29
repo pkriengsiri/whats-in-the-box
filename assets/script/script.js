ff36e7d27ce90b9b6308d7aabfe040578b3e81c6
Pete Kriengsiri <pkriengsiri@gmail.com>
2020-12-29 11:08:40 -0500

    fixes #25, added variables and function to store and retrieve username via localStorage

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,16 +2,26 @@ $(document).ready(function () {
   // DOM VARIABLES
 
   // JS VARIABLES
-  var userBoxes = []; // Tracks all the boxes the user has packed
+  var userBoxes = JSON.parse(localStorage.getItem("boxes")) || []; // Tracks all the boxes the user has packed
+  var storedUserName = localStorage.getItem("user-name") || ""; //Tracks the user name if the user has previously created a box
+
+  // FUNCTION DECLARATIONS
+  // Populates the name field in the form with the stored user name;
+  function init() {
+    if (storedUserName !== "") {
+      $("#user-name").val(storedUserName);
+    }
+  }
 
   // Handles the behavior once the user clicks the submit button
-
   function generateQRCode() {
-
     // Pulls text fields from form and stores them as variables
     var userName = $("#user-name").val();
     var boxName = $("#box-name").val();
     var boxContent = $("#box-content").val();
+
+    // Stores the user name in local storage
+    localStorage.setItem("user-name", userName);
 
     // Call the paste API to box the box contents to a new paste
     var queryURL =
@@ -30,13 +40,17 @@ $(document).ready(function () {
         ],
       },
 
-      
       success: function (response) {
-       
         // Get the paste ID from the submitted paste
         var pasteID = response.id;
-        var htmlLink = "https://pkriengsiri.github.io/whats-in-the-box/Print/index.html?" + pasteID;
-        $("#print-link").attr("href", "https://api.html2pdf.app/v1/generate?apiKey=flx8MMkbCefJ2A3NYSTRE53Wi0ZlvXtFem7hEKSTtFEOrb0PPiaQKXRuKqGThL8m&format=Letter&filename=QRLabel&url=" + htmlLink);
+        var htmlLink =
+          "https://pkriengsiri.github.io/whats-in-the-box/Print/index.html?" +
+          pasteID;
+        $("#print-link").attr(
+          "href",
+          "https://api.html2pdf.app/v1/generate?apiKey=flx8MMkbCefJ2A3NYSTRE53Wi0ZlvXtFem7hEKSTtFEOrb0PPiaQKXRuKqGThL8m&format=Letter&filename=QRLabel&url=" +
+            htmlLink
+        );
         console.log(pasteID);
         var myUrl = new URL(
           "https://pkriengsiri.github.io/whats-in-the-box/scan"
@@ -52,27 +66,22 @@ $(document).ready(function () {
           colorDark: "#000000",
           colorLight: "#ffffff",
           correctLevel: QRCode.CorrectLevel.H,
-        });    
-      }
+        });
+      },
     });
   }
 
- 
-    
   // FUNCTION CALLS
+  init();
+
   // EVENT HANDLERS
   //event listener to save input text into variables
   $("#print-label").on("click", function (e) {
     // Prevents default form behavior
     e.preventDefault();
     //reveals the Modal with QR code
-    $('#myModal').modal('show');
-    
+    $("#myModal").modal("show");
+
     generateQRCode();
-    
   });
-
-  
-
-
 });
