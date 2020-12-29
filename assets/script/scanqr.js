96b2121091c4de517fc93cc86e046a0a19377276
Pete Kriengsiri <pkriengsiri@gmail.com>
2020-12-28 20:41:15 -0500

    Fixes #23, added HTML layout and function for pulling paste ID from url

--- assets/script/scanqr.js
+++ assets/script/scanqr.js
@@ -2,10 +2,22 @@ $(document).ready(function() {
 //DOM VARIABLES
 
 //JS VARIABLES
+var userName = "";
+var boxName = "";
+var boxContents = "";
+var URL = window.location.href;
+var pasteID = "";
 
 //FUNCTION DECLARATIONS
+//Gets the paste ID from the URL
+function getPasteID() {
+    var urlArray = URL.split("=");
+    pasteID = urlArray[1];
+    console.log(pasteID);
+}
 
 //FUNCTION CALLS
+getPasteID();
 
 //EVENT HANDLERS
 
