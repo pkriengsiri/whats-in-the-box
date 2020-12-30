1ea67a9a6160c8cbc67fbe966ab068a98f786da3
Pete Kriengsiri <pkriengsiri@gmail.com>
2020-12-30 11:25:06 -0500

    Fixes #29, Changed button element to a element.  Added link to boxes contents display page on the a element.  Deleted event listener that is no longer used

--- assets/script/boxlist.js
+++ assets/script/boxlist.js
@@ -3,46 +3,21 @@ $(document).ready(function() {
 function renderButtons() {
 
   var userBoxes = JSON.parse(localStorage.getItem("boxes"));
-  for (var i = 0; i < userBoxes.length; i++) {
-    var listOfBoxes = $("<button>");
-    listOfBoxes.addClass("list-group-item list-button col-3");
+  for (var i = 0; i < userBoxes.length; i++) 
+  {
+    var paste = userBoxes[i].pasteID
+    var listOfBoxes = $("<a>");
+    listOfBoxes.addClass("btn list-group-item list-button col-3");
     listOfBoxes.text(userBoxes[i].name);
     listOfBoxes.attr("data-paste", userBoxes[i].pasteID);
+    listOfBoxes.attr("href","https://pkriengsiri.github.io/whats-in-the-box/scan/?name="+paste)
     listOfBoxes.css("text-align", "left");
     $("#list-boxes").prepend(listOfBoxes);
-    console.log(userBoxes[i].name);
-    var paste = userBoxes[i].pasteID
-    console.log(paste);
   }
 }
 
 
 
 renderButtons();
-
- $(document).on("click", ".list-button", function (e) {
-   var pasteID = $(this).attr("data-paste");
-   console.log($(this).attr("data-paste"));
-   getBoxInfo(pasteID);
-
-  function getBoxInfo(pasteID) {
-    var queryURL = "https://api.paste.ee/v1/pastes/" + pasteID + "/?key=aNXCh2y6HrufDC2QMvqoKW7in1uvG7AhUOP1Z4JGF"
-        $.ajax({
-            url: queryURL,
-            method: "GET",
-        }).then(function(response) {
-
-          // boxName = response.paste.description;
-          userName = response.paste.sections[0].name;
-          boxContents = response.paste.sections[0].contents;
-
-          $("#user-name").text(userName);
-          // $("#box-name").text(boxName);
-          $("#box-contents").text(boxContents);
-        });
-}
-
-    
-    });
 
 });
