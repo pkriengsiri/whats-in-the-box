02d20adc019767436ce848c0e0e6400dc4a40e98
mugi <munkhuu1233@gmail.com>
2020-12-29 19:44:53 -0500

    fixes/29 created index.html and boxlist.js files.

--- assets/script/script.js
+++ assets/script/script.js
@@ -13,18 +13,7 @@ $(document).ready(function () {
     }
   }
 
-  function renderButtons() {
-    for (var i = 0; i < userBoxes.length; i++) {
-      var nameBox = userBoxes[i];
-      var listOfBoxes = $("<button>");
-      listOfBoxes.addClass("list-group-item list-button col-3");
-      // listOfBoxes.attr("data-paste", nameBox.pasteID);
-      listOfBoxes.text(nameBox.name);
-      listOfBoxes.css("text-align", "left");
-      $("#box-name").prepend(listOfBoxes);
-      console.log(nameBox.name);
-    }
-  }
+  
 
   // Handles the behavior once the user clicks the submit button
   function generateQRCode() {
