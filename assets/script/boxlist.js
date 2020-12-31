c9f6ca9ff57062cfc0352afa104941b3d85a716e
Pete Kriengsiri <pkriengsiri@gmail.com>
2020-12-31 10:53:59 -0500

    Fixes #50, Changed list to allow for two buttons per li element.  Added button to reprint QR code.  Added text to buttons

--- assets/script/boxlist.js
+++ assets/script/boxlist.js
@@ -10,13 +10,20 @@ function renderButtons() {
   for (var i = 0; i < userBoxes.length; i++) 
   {
     var paste = userBoxes[i].pasteID
-    var listOfBoxes = $("<a>");
-    listOfBoxes.addClass("btn list-group-item list-button col-3");
-    listOfBoxes.text("userBoxes[i].name);
-    listOfBoxes.attr("data-paste", userBoxes[i].pasteID);
-    listOfBoxes.attr("href","https://pkriengsiri.github.io/whats-in-the-box/scan/?name="+paste)
-    listOfBoxes.css("text-align", "left");
-    $("#list-boxes").prepend(listOfBoxes);
+
+    var ulEl = $("<ul>");
+    ulEl.addClass("list-group");
+    var liEl = $("<li>");
+    liEl.addClass("list-group-item");
+    var boxDisplayButton = $("<a>");
+    boxDisplayButton.addClass("btn");
+    boxDisplayButton.text(userBoxes[i].name);
+    //listOfBoxes.attr("data-paste", userBoxes[i].pasteID);
+    boxDisplayButton.attr("href","https://pkriengsiri.github.io/whats-in-the-box/scan/?name="+paste);
+    //listOfBoxes.css("text-align", "left");
+    liEl.append(boxDisplayButton);
+    ulEl.append(liEl);
+    $("#list-boxes").append(ulEl);
   }
 }
 
