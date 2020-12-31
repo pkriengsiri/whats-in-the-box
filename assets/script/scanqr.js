360a4822489cb91eac66e4d0b4a5077ea037c585
Pete Kriengsiri <pkriengsiri@gmail.com>
2020-12-30 21:25:22 -0500

    Fixes #24, completed function to search for and display charities by zip code

--- assets/script/scanqr.js
+++ assets/script/scanqr.js
@@ -1,87 +1,128 @@
-$(document).ready(function() {
-//DOM VARIABLES
+$(document).ready(function () {
+  //DOM VARIABLES
 
-//JS VARIABLES
-var userName = "";
-var boxName = "";
-var URL = window.location.href;
-var pasteID = "";
+  //JS VARIABLES
+  var userName = "";
+  var boxName = "";
+  var URL = window.location.href;
+  var pasteID = "";
 
-//FUNCTION DECLARATIONS
-//Gets the paste ID from the URL
-function getPasteID() {
+  //FUNCTION DECLARATIONS
+  //Gets the paste ID from the URL
+  function getPasteID() {
     var urlArray = URL.split("=");
     pasteID = urlArray[1];
     console.log(pasteID);
-}
+  }
 
-//Pulls the box info from the paste and sets the content on the DOM
-function getBoxInfo() {
-    var queryURL = "https://api.paste.ee/v1/pastes/" + pasteID + "/?key=aNXCh2y6HrufDC2QMvqoKW7in1uvG7AhUOP1Z4JGF"
-        $.ajax({
-            url: queryURL,
-            method: "GET",
-        }).then(function(response) {
-          // Get username and box name from paste API JSON  
-          boxName = response.paste.description;
-          userName = response.paste.sections[0].name;
-          // Get box contents and parse stringified array
-          boxContentsArray = JSON.parse(response.paste.sections[0].contents);
+  //Pulls the box info from the paste and sets the content on the DOM
+  function getBoxInfo() {
+    var queryURL =
+      "https://api.paste.ee/v1/pastes/" +
+      pasteID +
+      "/?key=aNXCh2y6HrufDC2QMvqoKW7in1uvG7AhUOP1Z4JGF";
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      // Get username and box name from paste API JSON
+      boxName = response.paste.description;
+      userName = response.paste.sections[0].name;
+      // Get box contents and parse stringified array
+      boxContentsArray = JSON.parse(response.paste.sections[0].contents);
 
-          // Print each element of an array in the box-contents div
-          for(var i=0; i < boxContentsArray.length; i++) {
-              var pEl = $("<p>")
-              pEl.text(boxContentsArray[i]);
-              $("#box-contents").append(pEl);
-              pEl.addClass("mb-0");
-          }
+      // Print each element of an array in the box-contents div
+      for (var i = 0; i < boxContentsArray.length; i++) {
+        var pEl = $("<p>");
+        pEl.text(boxContentsArray[i]);
+        $("#box-contents").append(pEl);
+        pEl.addClass("mb-0");
+      }
 
-          
-          // Set contents for user name and box name divs
-          $("#user-name").text(userName);
-          $("#box-name").text(boxName);
-        });
-}
+      // Set contents for user name and box name divs
+      $("#user-name").text(userName);
+      $("#box-name").text(boxName);
+    });
+  }
 
-// Displays the list of charities within 10 miles of the user's zip
-function displayCharitiesByZip() {
+  // Displays the list of charities within 10 miles of the user's zip
+  function displayCharitiesByZip() {
     // Get the user zip code from the form
     zipCode = $("#zip-form-input").val();
 
     //Check to see if it's a number or 5 digits
-    if(zipCode.length <5 || isNaN(zipCode)) {
-        // Display error message
-        $("#zip-error").removeClass("d-none");
+    if (zipCode.length < 5 || isNaN(zipCode)) {
+      // Display error message
+      $("#zip-error").removeClass("d-none");
     } else {
-        // Hide any error messages
-        $("#zip-error").addClass("d-none");
+      // Hide any error messages
+      $("#zip-error").addClass("d-none");
+
+      // API call to org hunter api to get charities by zip
+      $.ajax({
+        url:
+          "https://cors-anywhere.herokuapp.com/http://data.orghunter.com/v1/charitysearch?user_key=219c0c9b2d181b353f5f69036fb5e105&eligible=1&distance=10&zipCode=" +
+          zipCode,
+        method: "GET",
+      }).then(function (response) {
+        console.log(response);
+        // Clear the list
+        $("#charity-list").empty();
+
+        // Display first 10 charities from the API call
+        for (var i = 0; i < 10; i++) {
+          charityLiEl = $("<li>");
+          charityLiEl.addClass("list-group-item");
+          charityNameEl = $("<h5>");
+          charityNameEl.text(response.data[i].charityName);
+          charityLiEl.append(charityNameEl);
+          charityURLEl = $("<a>");
+          charityURLEl.text("Website");
+          charityURLEl.attr("href", response.data[i].url);
+          charityURLEl.attr("target", "_blank");
+          charityLiEl.append(charityURLEl);
+          brEl = $("<br>");
+          charityLiEl.append(brEl);
+          charityMapsEl = $("<a>");
+          charityMapsEl.text("Google Maps");
+          charityMapsEl.attr(
+            "href",
+            "https://www.google.com/maps/search/?api=1&query=" +
+              response.data[i].latitude +
+              "," +
+              response.data[i].longitude
+          );
+          charityMapsEl.attr("target", "_blank");
+          charityLiEl.append(charityMapsEl);
+          $("#charity-list").append(charityLiEl);
+        }
+      });
     }
-}
+  }
 
-// Displays the list of charities within 10 miles of the user's zip
-function displayCharitiesByGeo() {
+  // Displays the list of charities within 10 miles of the user's zip
+  function displayCharitiesByGeo() {
     console.log(window);
-}
+  }
 
-//FUNCTION CALLS
-getPasteID();
-getBoxInfo();
+  //FUNCTION CALLS
+  getPasteID();
+  getBoxInfo();
 
-//EVENT HANDLERS
-$("#charity-button").on("click", function() {
+  //EVENT HANDLERS
+  $("#charity-button").on("click", function () {
     $("#charityModal").modal("show");
-});
+  });
 
-$("#close-button").on("click", function () {
+  $("#close-button").on("click", function () {
     $("#charityModal").modal("hide");
   });
 
-$("#modal-form").on("submit", function(e) {
+  $("#modal-form").on("submit", function (e) {
     e.preventDefault();
-})
+  });
 
-$("#zip-search").on("click", displayCharitiesByZip)
+  $("#zip-search").on("click", displayCharitiesByZip);
 
-$("#geo-search").on("click", displayCharitiesByGeo)
-
+  $("#geo-search").on("click", displayCharitiesByGeo);
 });
