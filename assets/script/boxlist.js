4a164e83092634a344c7ca91c0a0ca0dbd678dd5
Pete Kriengsiri <pkriengsiri@gmail.com>
2020-12-31 11:00:14 -0500

    Fixes #50, added comments and deleted unused lines of code

--- assets/script/boxlist.js
+++ assets/script/boxlist.js
@@ -1,34 +1,45 @@
-$(document).ready(function() {
-// FUNCTION DECLARATIONS
+$(document).ready(function () {
+  // FUNCTION DECLARATIONS
 
-// Render the list of boxes to the DOM
-function renderButtons() {
-  // Pulls and parses the stringified array of boxes from localStorage
-  var userBoxes = JSON.parse(localStorage.getItem("boxes"));
+  // Render the list of boxes to the DOM
+  function renderButtons() {
+    // Pulls and parses the stringified array of boxes from localStorage
+    var userBoxes = JSON.parse(localStorage.getItem("boxes"));
 
-  //Prints the boxes to the DOM
-  for (var i = 0; i < userBoxes.length; i++) 
-  {
-    var paste = userBoxes[i].pasteID
+    //Prints the boxes to the DOM
+    for (var i = 0; i < userBoxes.length; i++) {
+      var paste = userBoxes[i].pasteID;
+      var htmlLink =
+        "https://pkriengsiri.github.io/whats-in-the-box/Print/index.html?" +
+        paste;
 
-    var ulEl = $("<ul>");
-    ulEl.addClass("list-group");
-    var liEl = $("<li>");
-    liEl.addClass("list-group-item");
-    var boxDisplayButton = $("<a>");
-    boxDisplayButton.addClass("btn");
-    boxDisplayButton.text(userBoxes[i].name);
-    //listOfBoxes.attr("data-paste", userBoxes[i].pasteID);
-    boxDisplayButton.attr("href","https://pkriengsiri.github.io/whats-in-the-box/scan/?name="+paste);
-    //listOfBoxes.css("text-align", "left");
-    liEl.append(boxDisplayButton);
-    ulEl.append(liEl);
-    $("#list-boxes").append(ulEl);
+      // Creates a ul, creates a li, creates two buttons for each li, then appends all to the DOM
+      var ulEl = $("<ul>");
+      ulEl.addClass("list-group");
+      var liEl = $("<li>");
+      liEl.addClass("list-group-item");
+      var boxDisplayButton = $("<a>");
+      boxDisplayButton.addClass("btn");
+      boxDisplayButton.text("Box Name: " + userBoxes[i].name);
+      boxDisplayButton.attr(
+        "href",
+        "https://pkriengsiri.github.io/whats-in-the-box/scan/?name=" + paste
+      );
+      liEl.append(boxDisplayButton);
+      var reprintQRButton = $("<a>");
+      reprintQRButton.addClass("btn");
+      reprintQRButton.text("Reprint QR Code");
+      reprintQRButton.attr(
+        "href",
+        "https://api.html2pdf.app/v1/generate?apiKey=flx8MMkbCefJ2A3NYSTRE53Wi0ZlvXtFem7hEKSTtFEOrb0PPiaQKXRuKqGThL8m&format=Letter&filename=QRLabel&url=" +
+          htmlLink
+      );
+      liEl.append(reprintQRButton);
+      ulEl.append(liEl);
+      $("#list-boxes").append(ulEl);
+    }
   }
-}
 
-
-// FUNCTION CALLS
-renderButtons();
-
+  // FUNCTION CALLS
+  renderButtons();
 });
